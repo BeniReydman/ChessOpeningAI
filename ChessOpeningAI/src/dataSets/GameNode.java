048f7dfec82478038aca5937782fee3c2f34320b
Beni <benireydman901@hotmail.com>
2019-07-10 15:45:06 +0900

    Additional Classes

--- ChessOpeningAI/src/dataSets/GameNode.java
+++ ChessOpeningAI/src/dataSets/GameNode.java
@@ -1,5 +1,21 @@
 package dataSets;
 
 public class GameNode {
+	
+	GameNode next;
 
+	public GameNode(GameNode next)
+	{
+		this.next = next;
+	}
+	
+	public void setNext(GameNode next)
+	{
+		this.next = next;
+	}
+	
+	public GameNode getNext()
+	{
+		return next;
+	}
 }
